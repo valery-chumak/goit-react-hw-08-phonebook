1526553584206ec029030087a963a62cc0a7b01f
valery-chumak <blossssom88@gmail.com>
2022-11-10 22:37:10 +0200

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,4 @@
-import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
+import { configureStore } from '@reduxjs/toolkit';
 
 // reducers
 import contactsReducer from './contacts/contacts-slice';
