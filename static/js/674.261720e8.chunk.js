56bd2437fe94dae03960d60b45bb9c12b080e49a
valery-chumak <72942456+valery-chumak@users.noreply.github.com>
2022-11-10 21:32:44 +0000

    Deploying to gh-pages from @ valery-chumak/goit-react-hw-08-phonebook@c6e99607b5345f8e718bf6e0e28601b1fe497fcf 🚀

---
+++ static/js/674.261720e8.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[674],{8674:function(e,t,a){a.r(t),a.d(t,{default:function(){return s}});var n="HomePage_heroSection__cjh5j",o="HomePage_heroContainer__g2ZS7",r="HomePage_heroContainerTitle__clAUP",c=a(184),s=function(){return(0,c.jsx)(c.Fragment,{children:(0,c.jsxs)("section",{className:n,children:[(0,c.jsx)("div",{className:o}),(0,c.jsx)("h1",{className:r,children:"Contact Book"})]})})}}}]);
+//# sourceMappingURL=674.261720e8.chunk.js.map
